24a594723b297ea53a3f1c1c49eb7d88ae830ab2
Devansh Choudaha <41153189+DevanshChoudaha@users.noreply.github.com>
2021-06-03 13:10:12 +0530

    Connect with MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server started at port ${ PORT }'));
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
